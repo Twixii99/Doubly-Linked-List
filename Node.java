93bc7a516d34562bb019216d5fc16738e47cbca1
Mahmoud kamal <42426976+lilkamal@users.noreply.github.com>
2019-04-19 21:55:05 +0200

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,12 @@
+package let;
+
+public class Node {
+	public Object data;
+	public Node next = null;
+	
+	public Node(Object data) {
+		// TODO Auto-generated constructor stub
+		this.data = data;
+	}
+
+}
