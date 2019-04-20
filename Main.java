995042e89bcf705ce3d9f7b18ee2d73728e25ade
Mahmoud kamal <42426976+lilkamal@users.noreply.github.com>
2019-04-20 20:53:51 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,8 +2,6 @@ package let;
 import java.util.Scanner;
 
 public class Main {
-	
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			
@@ -16,7 +14,7 @@ public class Main {
 			Scanner take3 = new Scanner(System.in);
 			 for(;;) {
 			System.out.println("ensert the operation");
-		    int choose = take3.nextInt();
+		    int choose = input.nextInt();
 		   
 		    switch(choose) {
 		    case(1):{
@@ -29,9 +27,11 @@ public class Main {
 		    	break;
 		    }
 		    case(2):{
+                        System.out.println("enter name");
 		    	char ch = input.next().charAt(0);
 		    	if(!p1.l1.contains(ch)) break;
-		    	System.out.println(p1.evaluatePolynomial(ch, take.nextInt()));
+                        System.out.println("enter value");
+		    	System.out.println(p1.evaluatePolynomial(ch, input.nextInt()));
 		    	break;
 		    }
 		    case(3):{
@@ -41,43 +41,46 @@ public class Main {
 		    	char c2 = take2.nextLine().charAt(0);
 		    	if(!p1.l1.contains(c1) && !p1.l1.contains(c2)) break;
 		    	int [][] ans = p1.add(c1, c2);
-		    	p1.l1.remove('R');
-		    	p1.setPolynomial('R',ans);
+		    	p1.l1.modify('R',ans);
 		    	String ss = "";
 		    	for(int i = 0; i < ans.length; i++)
 		    		ss += "(" + ans[i][0] + "," + ans[i][1] + ")";
 		    	System.out.println(ss);
+                        break;
 		    }
 		    case(4):{
-		    	char c1 = input.nextLine().charAt(0);
-		    	char c2 = input.nextLine().charAt(0);
+		    	System.out.println("c1");
+		    	char c1 = take.nextLine().charAt(0);
+		    	System.out.println("c2");
+		    	char c2 = take2.nextLine().charAt(0);
 		    	if(!p1.l1.contains(c1) & !p1.l1.contains(c2)) break;
 		    	int [][] ans1 = p1.subtract(c1,c2);
-		    	p1.l1.remove('R');
-		    	p1.setPolynomial('R',ans1);
+		    	p1.l1.modify('R',ans1);
 	    		for(int i = 0; i < ans1.length; i++)
 	    			System.out.println("(" + ans1[i][0] + "," + ans1[i][1] + ")");
 		    	break;
 		    }
 		    case(5):{
-		    	char c1 = input.nextLine().charAt(0);
-		    	char c2 = input.nextLine().charAt(0);
+		    	System.out.println("c1");
+		    	char c1 = take.nextLine().charAt(0);
+		    	System.out.println("c2");
+		    	char c2 = take2.nextLine().charAt(0);
 		    	if(!p1.l1.contains(c1) & !p1.l1.contains(c2)) break;
 		    	int [][] ans2 = p1.multiply(c1,c2);
-		   		p1.l1.remove('R');
-		   		p1.setPolynomial('R',ans2);
+		   	p1.l1.modify('R',ans2);
 	    		for(int i = 0; i < ans2.length; i++)
 	    			System.out.println("(" + ans2[i][0] + "," + ans2[i][1] + ")");
 	    		break;
 		    }
 		    case(6):{
-		    	char c1 = input.nextLine().charAt(0);
+                        System.out.println("enter name");
+		    	char c1 = take2.nextLine().charAt(0);
 		    	if(!p1.l1.contains(c1)) break;
-		    	System.out.println(p1.evaluatePolynomial(c1, take2.nextFloat()));
+		    	System.out.println(p1.print(c1));
 		    	break;
 		    }
 		    case(7):{
-		    	char c1 = input.nextLine().charAt(0);
+		    	char c1 = take.nextLine().charAt(0);
 		    	if(!p1.l1.contains(c1)) break;
 		    	p1.clearPolynomial(c1);
 		    	break;
